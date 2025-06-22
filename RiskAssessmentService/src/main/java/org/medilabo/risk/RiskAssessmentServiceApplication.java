45a398c691d3ab68ac1e5b8199ef9156dccef1cb
pierre-edouard iung <contact@pierre-edouard.tech>
2025-06-22 17:30:57 +0200

    feat(sprint-3): init risk assessment microservice

---
+++ RiskAssessmentService/src/main/java/org/medilabo/risk/RiskAssessmentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.medilabo.risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAssessmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskAssessmentServiceApplication.class, args);
+    }
+
+}
---
+++ RiskAssessmentService/src/test/java/org/medilabo/riskassessmentservice/RiskAssessmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medilabo.riskassessmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskAssessmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
